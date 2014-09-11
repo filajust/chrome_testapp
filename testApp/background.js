bc18df18b48a30e95d6be791db85103a41a5ff6a
TechSmith <cse498@cse498-0501-techsmith.cse.msu.edu>
2014-09-11 15:25:41 -0400

    button to functions actually happening - works

--- testApp/background.js
+++ testApp/background.js
@@ -1,3 +1,23 @@
+chrome.extension.onMessage.addListener(
+  function(request, sender, sendResponse) {
+    switch (request.directive) {
+      case "one":
+      case "buttonTwo":
+      case "buttonThree":
+      case "buttonFour":
+      case "buttonFive":
+      case "buttonSix":
+      case "buttonSeven":
+      case "buttonEight":
+      case "buttonNine":
+        sendResponse({}); // sends back empty response
+        break;
+      default:
+        alert("Unmatched request of ;" + request + "' from script to background.js from" + sender);
+      }
+    }
+);
+
 chrome.app.runtime.onLaunched.addListener(function() {
   chrome.app.window.create('window.html', {
     'bounds': {
@@ -5,4 +25,4 @@ chrome.app.runtime.onLaunched.addListener(function() {
       'height': 500 
     }
   });
-});
+})
--- testApp/calculator.js
+++ testApp/calculator.js
@@ -1,7 +1,33 @@
 var cur = 0;
 var stored = 0;
 
-document.getElementById("buttonOne").addEventListener("clickOne", one);
+document.addEventListener('DOMContentLoaded', function () {
+  document.getElementById('one').addEventListener('click', one);
+})
+document.addEventListener('DOMContentLoaded', function () {
+  document.getElementById('buttonTwo').addEventListener('click', two);
+})
+document.addEventListener('DOMContentLoaded', function () {
+  document.getElementById('buttonThree').addEventListener('click', three);
+})
+document.addEventListener('DOMContentLoaded', function () {
+  document.getElementById('buttonFour').addEventListener('click', four);
+})
+document.addEventListener('DOMContentLoaded', function () {
+  document.getElementById('buttonFive').addEventListener('click', five);
+})
+document.addEventListener('DOMContentLoaded', function () {
+  document.getElementById('buttonSix').addEventListener('click', six);
+})
+document.addEventListener('DOMContentLoaded', function () {
+  document.getElementById('buttonSeven').addEventListener('click', seven);
+})
+document.addEventListener('DOMContentLoaded', function () {
+  document.getElementById('buttonEight').addEventListener('click', eight);
+})
+document.addEventListener('DOMContentLoaded', function () {
+  document.getElementById('buttonNine').addEventListener('click', nine);
+})
 
 function addNumber(num, concatNum) {
     return num * 10 + concatNum;
@@ -11,47 +37,65 @@ function displayNumber(num) {
   document.getElementById("result").innerHTML = num;
 }
 
-function one() {
+function one(e) {
+  chrome.extension.sendMessage({directive: "one"}, function(response) {
     cur = addNumber(cur, 1);
     displayNumber(cur);
+  });
 }
 
-function two() {
+function two(e) {
+  chrome.extension.sendMessage({directive: "buttonTwo"}, function(response) {
     cur = addNumber(cur, 2);
     displayNumber(cur);
+  });
 }
 
-function three() {
+function three(e) {
+  chrome.extension.sendMessage({directive: "buttonThree"}, function(response) {
     cur = addNumber(cur, 3);
     displayNumber(cur);
+  });
 }
 
-function four() {
+function four(e) {
+  chrome.extension.sendMessage({directive: "buttonFour"}, function(response) {
     cur = addNumber(cur, 4);
     displayNumber(cur);
+  });
 }
 
-function five() {
+function five(e) {
+  chrome.extension.sendMessage({directive: "buttonFive"}, function(response) {
     cur = addNumber(cur, 5);
     displayNumber(cur);
+  });
 }
 
-function six() {
+function six(e) {
+  chrome.extension.sendMessage({directive: "buttonSix"}, function(response) {
     cur = addNumber(cur, 6);
     displayNumber(cur);
+  });
 }
 
-function seven() {
+function seven(e) {
+  chrome.extension.sendMessage({directive: "buttonSeven"}, function(response) {
     cur = addNumber(cur, 7);
     displayNumber(cur);
+  });
 }
 
-function eight() {
+function eight(e) {
+  chrome.extension.sendMessage({directive: "buttonEight"}, function(response) {
     cur = addNumber(cur, 8);
     displayNumber(cur);
+  });
 }
 
-function nine() {
+function nine(e) {
+  chrome.extension.sendMessage({directive: "buttonNine"}, function(response) {
     cur = addNumber(cur, 9);
     displayNumber(cur);
+  });
 }
